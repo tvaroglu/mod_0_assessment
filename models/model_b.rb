f56a53cd882feebf332fe96fca1cef3bfec73c59
Taylor Varoglu <tava8398@colorado.edu>
2021-04-02 12:29:04 -0600

    Add developer hash to model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,43 @@
+developer = {
+  :first_name => 'Taylor',
+  :last_name => 'Varoglu',
+  :number_of_public_repos => 11,
+  :months_studying_programming => 16.5,
+  :is_motivated => true,
+  :is_organized => true,
+  :languages_familiar_with => [
+    {
+      :name => 'Python',
+      :scale_1_to_10 => 5,
+      :projects_worked_on => [
+        'Simple Salesforce API', 'Salesforce Bulk API', 'Reading & Writing Files'
+      ]
+    },
+    {
+      :name => 'ES6',
+      :scale_1_to_10 => 4,
+      :projects_worked_on => [
+        'Postman API Testing', 'Self-Guided Study'
+      ]
+    },
+    {
+      :name => 'HTML',
+      :scale_1_to_10 => 3,
+      :projects_worked_on => [
+        'Front End Development (via Bootstrap)', 'Self-Guided Study'
+      ]
+    },
+    {
+      :name => 'Ruby',
+      :scale_1_to_10 => 2,
+      :projects_worked_on => [
+        'Turing School Exercises', 'Self-Guided Study'
+      ]
+    }
+  ],
+  :industries_worked_in => [
+    'Telecommunications', 'Retail', 'Advertising'
+  ]
+}
+
+p developer
