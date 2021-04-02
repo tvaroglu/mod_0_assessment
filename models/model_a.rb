2dfc7a9981bcbf142147ae913f8e3bf0bce5db59
Taylor Varoglu <tava8398@colorado.edu>
2021-04-02 12:09:53 -0600

    Add dinosaur hash to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,16 @@
+dinosaur = {
+  :kingdom => 'Animalia',
+  :phylum => 'Chordata',
+  :max_height_in_ft => 40,
+  :max_weight_in_tons => 15.4,
+  :is_carnivor => true,
+  :is_apex_predator => true,
+  :prey => [
+    'Hadrosaurs', 'Ceratopsians', 'Ankylosaurs', 'Sauropods'
+  ],
+  :ecosystems_lived_in => [
+    'inland', 'coastal subtropical', 'semi-arid plains'
+  ]
+}
+
+p dinosaur
